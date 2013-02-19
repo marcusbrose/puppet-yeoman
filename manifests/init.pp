d44035c31969d3d5b4f5f7e723f383b75f528bb4
Nick Hartjes <nick@digibranding.nl>
2013-02-19 18:59:06 +0100

    Added some requires

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,32 @@
 class yeoman {
-	$yeomanPackages = [ "curl", "git", "rubygems", "libjpeg-turbo-progs", "optipng", "phantomjs", "python-software-properties" ]
-  	package { $yeomanPackages:
-    	ensure => "installed",
-    	require => Exec['apt-get update'],
-  	}
-    
-  	exec { 'install compass using npm':
-    	command => 'npm install -g compass',
-    	require => Package["npm"],
-  	}
-  	exec {
-        "install yeoman using npm":
-            command => "npm install -g yo grunt-cli bower",
-			require => Package["npm"],
+  $yeomanPackages = [ "curl", "git", "rubygems", "libjpeg-turbo-progs", "optipng", "phantomjs", "python-software-properties" ]
+    package { $yeomanPackages:
+      ensure => "installed",
+      require => Exec['apt-get update'],
+    }
+    package { 'compass':
+      ensure   => latest,
+      provider => 'gem',
+      require  =>  Package["rubygems"],
+    }
+    package { 'yo':
+      ensure => present,
+      provider => 'npm',
+      require => Class["nodejs"],
+    }
+    package { 'grunt-cli':
+      ensure => present,
+      provider => 'npm',
+      require => Class["nodejs"],
+    }
+    package { 'bower':
+      ensure => present,
+      provider => 'npm',
+      require => Class["nodejs"],
     }
     exec {
-        "checking yeoman":
-            command => "curl -L get.yeoman.io | bash",
-			require => Class["nodejs"],
+      "checking yeoman":
+      command => "curl -L get.yeoman.io | bash",
+      require => Class["nodejs"],
     }
 }
