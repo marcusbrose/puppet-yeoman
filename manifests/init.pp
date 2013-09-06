299b0917a942d37e18eefd8bed6132af6d9cf1a5
Marcus Brose <contact@marcusbrose.com>
2013-09-06 16:42:42 +0200

    always make sure to have the latest version of npm installed
    using node version manager https://npmjs.org/package/n

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,41 @@
 class yeoman {
-  $yeomanPackages = [ "curl", "git", "rubygems", "libjpeg-turbo-progs", "optipng", "phantomjs", "python-software-properties" ]
-    package { $yeomanPackages:
-      ensure => "installed",
-      require => Exec['apt-get update'],
-    }
-    package { 'compass':
-      ensure   => latest,
-      provider => 'gem',
-      require  =>  Package["rubygems"],
-    }
-    package { 'yo':
-      ensure => present,
-      provider => 'npm',
-      require => Class["nodejs"],
-    }
-    package { 'grunt-cli':
-      ensure => present,
-      provider => 'npm',
-      require => Class["nodejs"],
-    }
-    package { 'bower':
-      ensure => present,
-      provider => 'npm',
-      require => Class["nodejs"],
-    }
-    exec {
-      "checking yeoman":
-      command => "curl -L get.yeoman.io | bash",
-      require => Class["nodejs"],
-    }
+  $yeomanPackages = [ "git", "rubygems", "libjpeg-turbo-progs", "optipng", "phantomjs" ]
+  package { $yeomanPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+  package { 'compass':
+    ensure   => latest,
+    provider => 'gem',
+    require  =>  Package["rubygems"],
+  }
+  package { 'yo':
+    ensure => present,
+    provider => 'npm',
+    require => [Class["nodejs"], Exec['update npm']],
+  }
+  package { 'grunt-cli':
+    ensure => present,
+    provider => 'npm',
+    require => [Class["nodejs"], Exec['update npm']],
+  }
+  package { 'bower':
+    ensure => present,
+    provider => 'npm',
+    require => [Class["nodejs"], Exec['update npm']],
+  }
+  package { 'n':
+    ensure => present,
+    provider => 'npm',
+  }
+  exec {
+    'checking yeoman':
+    command => "curl -L get.yeoman.io | bash",
+    require => [Class["nodejs"], Class['other']],
+  }
+  exec { 
+    'update npm':
+    command => "n latest",
+    require => Package["n"],
+  }
 }
